ee114a6f973690af256247dc6b374f32bf97bf05
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-04-09 20:16:25 -0300

    configuring safe producer properties

--- src/main/java/com/kafka/twitter/producer/TwitterProducer.java
+++ src/main/java/com/kafka/twitter/producer/TwitterProducer.java
@@ -51,7 +51,6 @@ public class TwitterProducer {
 
 		// kafka producer
 		KafkaProducer<String, String> producer = createKafkaProducer();
-		
 
 		// send tweets to kafka
 		while (!client.isDone()) {
@@ -84,6 +83,12 @@ public class TwitterProducer {
 		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, ConfigConstants.bootstrapServer);
 		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		// create safe
+		properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+		properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+		properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+		properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
 		// create Producer
 		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
